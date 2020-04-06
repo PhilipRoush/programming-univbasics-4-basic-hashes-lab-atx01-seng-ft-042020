433ac66aacd43c3b2b22d7a885e7950ca1299f42
Philip Roush <roush.philip@gmail.com>
2020-04-06 12:29:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,11 +7,12 @@ def my_hash
 end
 
 def pioneer
-  
+  { name: "Grace Hopper" }
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+  
   # return a hash with a key :id assigned to the provided number
 end
